4f6c44f19e1198a6ddaf938a7d53ef5accb9ca79
ControlFlow <shvedov89@gmail.com>
2014-12-29 19:46:47 +0300

    0.9.5, finally

--- ReSharper.HeapView/Properties/AssemblyInfo.cs
+++ ReSharper.HeapView/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.9.4")]
+[assembly: AssemblyVersion("0.9.5")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 #if RESHARPER8
