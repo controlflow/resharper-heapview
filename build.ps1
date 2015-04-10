ba561ddaffee3da11550d22101258c8a1f276da7
ControlFlow <shvedov89@gmail.com>
2015-04-10 20:21:35 +0300

    hmm

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ $nuspec_file = 'ReSharper.HeapView.nuspec'
 $package_id = 'ReSharper.HeapView'
 
 nuget pack $nuspec_file -Exclude 'ReSharper.HeapView\bin.R90\**' -Properties "Configuration=$config;ReSharperDep=ReSharper;ReSharperVer=8.1;PackageId=$package_id"
-nuget pack $nuspec_file -Exclude 'ReSharper.HeapView\bin.R8*\**' -Properties "Configuration=$config;ReSharperDep=Wave;ReSharperVer=[1.0];PackageId=$package_id.R90"
+nuget pack $nuspec_file -Exclude 'ReSharper.HeapView\bin.R8*\**' -Properties "Configuration=$config;ReSharperDep=Wave;ReSharperVer=[1.0,2.0];PackageId=$package_id.R90"
