93640331b11f6d29ad92d4a6dae28febe243cc83
ControlFlow <shvedov89@gmail.com>
2015-04-11 19:57:20 +0300

    AssemblyInfo.cs

--- ReSharper.HeapView/Properties/AssemblyInfo.cs
+++ ReSharper.HeapView/Properties/AssemblyInfo.cs
@@ -1,6 +1,12 @@
 using System.Reflection;
 #if RESHARPER8
+
 using JetBrains.Application.PluginSupport;
+
+[assembly: PluginTitle("ReSharper Heap Allocations Viewer")]
+[assembly: PluginDescription("Heap Allocations Viewer plugin for ReSharper")]
+[assembly: PluginVendor("Shvedov Alexander")]
+
 #endif
 
 [assembly: AssemblyTitle("ReSharper.HeapView")]
@@ -8,17 +14,9 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Shvedov Alexander")]
 [assembly: AssemblyProduct("ReSharper Heap Allocations Viewer")]
-[assembly: AssemblyCopyright("Copyright © Shvedov Alexander, 2013")]
+[assembly: AssemblyCopyright("Copyright © Shvedov Alexander, 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.9.6")]
+[assembly: AssemblyVersion("0.9.7")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-#if RESHARPER8
-
-[assembly: PluginTitle("ReSharper Heap Allocations Viewer")]
-[assembly: PluginDescription("Heap Allocations Viewer plugin for ReSharper")]
-[assembly: PluginVendor("Shvedov Alexander")]
-
-#endif
