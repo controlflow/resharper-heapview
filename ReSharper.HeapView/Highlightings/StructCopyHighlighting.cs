8f338551bef659b66d8d9257fa54563539f21cb5
ControlFlow <shvedov89@gmail.com>
2015-04-17 18:40:44 +0300

    Working on struct copy analyzer

--- ReSharper.HeapView/Highlightings/StructCopyHighlighting.cs
+++ ReSharper.HeapView/Highlightings/StructCopyHighlighting.cs
@@ -9,29 +9,29 @@ using JetBrains.ReSharper.Feature.Services.Daemon;
 #endif
 // ReSharper disable MemberCanBePrivate.Global
 
-//[assembly: RegisterConfigurableSeverity(
-//  id: StructCopyHighlighting.SEVERITY_ID,
-//  compoundItemName: null,
-//  group: AllocationHighlightingGroupIds.ID,
-//  title: "Struct copy",
-//  description: "Highlights expressions where struct copying happens",
-//  defaultSeverity: Severity.HINT,
-//  solutionAnalysisRequired: false)]
+[assembly: RegisterConfigurableSeverity(
+  id: StructCopyHighlighting.SEVERITY_ID,
+  compoundItemName: null,
+  group: AllocationHighlightingGroupIds.ID,
+  title: "Struct copy",
+  description: "Highlights expressions where struct copying happens",
+  defaultSeverity: Severity.HINT,
+  solutionAnalysisRequired: false)]
 
 namespace JetBrains.ReSharper.HeapView.Highlightings
 {
-  //[ConfigurableSeverityHighlighting(
-  //  configurableSeverityId: SEVERITY_ID,
-  //  languages: CSharpLanguage.Name,
-  //  AttributeId = Compatibility.STRUCT_COPY_ID,
-  //  ShowToolTipInStatusBar = false,
-  //  ToolTipFormatString = MESSAGE)]
-  //public class StructCopyHighlighting : PerformanceHighlightingBase
-  //{
-  //  public const string SEVERITY_ID = "HeapView.StructCopy";
-  //  public const string MESSAGE = "Struct copy: {0}";
-  //
-  //  public StructCopyHighlighting([NotNull] ITreeNode element, [NotNull] string description)
-  //    : base(element, MESSAGE, description) { }
-  //}
+  [ConfigurableSeverityHighlighting(
+    configurableSeverityId: SEVERITY_ID,
+    languages: CSharpLanguage.Name,
+    AttributeId = Compatibility.STRUCT_COPY_ID,
+    ShowToolTipInStatusBar = false,
+    ToolTipFormatString = MESSAGE)]
+  public class StructCopyHighlighting : PerformanceHighlightingBase
+  {
+    public const string SEVERITY_ID = "HeapView.StructCopy";
+    public const string MESSAGE = "Struct copy: {0}";
+  
+    public StructCopyHighlighting([NotNull] ITreeNode element, [NotNull] string description)
+      : base(element, MESSAGE, description) { }
+  }
 }
