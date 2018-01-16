f83c2e47756840d4ff0b4c5aee6af1eb577be761
Alexander.Kirsanov <alexander.kirsanov@jetbrains.com>
2018-01-16 23:28:12 +0300

    Added highlighter group and demo text for Color Scheme settings page

--- ReSharper.HeapView/src/Highlightings/HeapViewAttributeIds.cs
+++ ReSharper.HeapView/src/Highlightings/HeapViewAttributeIds.cs
@@ -2,6 +2,8 @@
 {
   public static class HeapViewAttributeIds
   {
+    public const string GROUP_ID = "ReSharper HeapView Boxing";
+
     public const string BOXING_HIGHLIGHTING_ID = "ReSharper HeapView Boxing";
     public const string ALLOCATION_HIGHLIGHTING_ID = "ReSharper HeapView Allocation";
     public const string STRUCT_COPY_ID = "ReSharper HeapView Struct copy";
