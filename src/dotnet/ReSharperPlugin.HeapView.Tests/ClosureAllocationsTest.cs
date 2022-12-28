3fa814d3ed0231ccb3dc9cb035273b9438c937da
Alexander.Shvedov <alexander.shvedov@jetbrains.com>
2022-12-28 16:06:56 +0100

    Hopefully, finish with #96, implement optimization opt-out via explicit delegate creation, better highlighting ranges, test with method group variance

--- src/dotnet/ReSharperPlugin.HeapView.Tests/ClosureAllocationsTest.cs
+++ src/dotnet/ReSharperPlugin.HeapView.Tests/ClosureAllocationsTest.cs
@@ -50,6 +50,8 @@ public abstract class ClosureAllocationsTestBase : CSharpHighlightingTestBase
   [LatestSupportedCSharpLanguageLevel(CSharpLanguageLevel.CSharp100)]
   [Test] public void TestMethodGroup04() { DoNamedTest2(); }
   [Test] public void TestMethodGroup05() { DoNamedTest2(); }
+  [Test] public void TestMethodGroup06() { DoNamedTest2(); }
+  [Test] public void TestMethodGroup07() { DoNamedTest2(); }
 }
 
 [TestNetFramework46]
--- src/dotnet/ReSharperPlugin.HeapView/Analyzers/AllocationOfMethodGroupDelegateAnalyzer.cs
+++ src/dotnet/ReSharperPlugin.HeapView/Analyzers/AllocationOfMethodGroupDelegateAnalyzer.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using JetBrains.Annotations;
 using JetBrains.ReSharper.Daemon.CSharp.Stages;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Psi;
@@ -6,6 +7,7 @@ using JetBrains.ReSharper.Psi.CSharp;
 using JetBrains.ReSharper.Psi.CSharp.DeclaredElements;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.ReSharper.Psi.CSharp.Util;
+using JetBrains.ReSharper.Psi.Tree;
 using ReSharperPlugin.HeapView.Highlightings;
 
 namespace ReSharperPlugin.HeapView.Analyzers;
@@ -35,8 +37,13 @@ public class AllocationOfMethodGroupDelegateAnalyzer : HeapAllocationAnalyzerBas
     if (referenceExpression.IsInTheContextWhereAllocationsAreNotImportant())
       return;
 
+    var isExplicitDelegateCreation = IsSurroundedByExplicitDelegateCreationExpression(referenceExpression, out var explicitCreationNode);
+
     if (parametersOwner is IMethod { IsStatic: true } or ILocalFunction { IsStatic: true }
-        && data.GetLatestSupportedLanguageLevel() >= CSharpLanguageLevel.CSharp110)
+        && data.GetLatestSupportedLanguageLevel() >= CSharpLanguageLevel.CSharp110
+        // check for explicit delegate allocation: `new Action(StaticMethod)`
+        // see https://github.com/dotnet/roslyn/issues/62832#issuecomment-1201144122
+        && !isExplicitDelegateCreation)
     {
       // C# 11 caches delegate instances from static methods and static local functions
       return;
@@ -44,6 +51,26 @@ public class AllocationOfMethodGroupDelegateAnalyzer : HeapAllocationAnalyzerBas
 
     var delegateTypeText = delegateType.GetPresentableName(referenceExpression.Language, CommonUtils.DefaultTypePresentationStyle);
     consumer.AddHighlighting(new DelegateAllocationHighlighting(
-      referenceExpression.NameIdentifier, $"new '{delegateTypeText}' instance creation"));
+      explicitCreationNode ?? referenceExpression.NameIdentifier,
+      $"new '{delegateTypeText}' instance creation"));
+  }
+
+  [Pure]
+  private static bool IsSurroundedByExplicitDelegateCreationExpression(IReferenceExpression methodGroupReference, out ITreeNode? nodeToHighlight)
+  {
+    var containingExpression = methodGroupReference.GetContainingParenthesizedExpression();
+    var argument = CSharpArgumentNavigator.GetByValue(containingExpression);
+
+    var objectCreationExpression = ObjectCreationExpressionNavigator.GetByArgument(argument);
+    if (objectCreationExpression != null
+        && objectCreationExpression.ArgumentsEnumerable.SingleItem == argument
+        && objectCreationExpression.Type() is IDeclaredType (IDelegate))
+    {
+      nodeToHighlight = objectCreationExpression.NewKeyword;
+      return true;
+    }
+
+    nodeToHighlight = null;
+    return false;
   }
 }
